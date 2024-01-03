4aded9a1cd04725724fc8fdf5fe51ecfccc1cb34
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2024-01-03 10:37:52 -0500

    Primer commit

---
+++ app/src/main/java/com/rycry/progressbar/MainActivity.java
@@ -0,0 +1,55 @@
+package com.rycry.progressbar;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private Button startButton;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Inicia un hilo para simular un proceso en segundo plano
+                new Thread(new Runnable() {
+                    public void run() {
+                        while (progressStatus < 100) {
+                            progressStatus += 1;
+
+                            // Actualiza la barra de progreso en el hilo principal
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressStatus);
+                                }
+                            });
+
+                            try {
+                                // Agrega una pequeña pausa para simular el progreso
+                                Thread.sleep(50);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+}
